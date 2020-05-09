92ffebdc18ba607cefccb8c40fdbfdf8ac8b5029
Damien Gallagher <damiengallagher@Damiens-MacBook-Pro-16.local>
2020-05-09 10:50:31 +0100

    Initial commit

---
+++ src/main/java/com/jenkins/developer/simpleapp/SpringBootApplicationUsingJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.developer.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationUsingJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationUsingJenkinsApplication.class, args);
+	}
+
+}
